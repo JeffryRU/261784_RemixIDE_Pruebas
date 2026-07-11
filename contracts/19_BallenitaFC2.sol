2629d6c9e85c9365051600d04b4e7cbf4b3c3692
JeffryRU <jeffry.25.ru@gmail.com>
2026-07-11 15:31:29 -0500

    261784_Examen

--- contracts/19_BallenitaFC2.sol
+++ contracts/19_BallenitaFC2.sol
@@ -3,12 +3,14 @@ pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
 contract BallenitaFCToken is ERC20, Ownable {
 
     uint256 public precioToken = 0.001 ether; //es igual a 1000000000000000
 
-    constructor() ERC20("Ballenita Futbol Club Token", "BFT") Ownable(msg.sender) {
+    constructor() ERC20("Ballenita Futbol Club Token", "BFT") Ownable(msg.sender) { 
+             
         _mint(owner(), 1000 * 10**18);
     }
 
@@ -25,14 +27,21 @@ contract BallenitaFCToken is ERC20, Ownable {
     }
 
     function comprarTokens() public payable { //me mandan 0.000000000001 tokens
+        console.log("Ejecutado por Luis Jose Flores Rodriguez");
         require(msg.value > 0, "Debes enviar un valor minimo de ETH");
         uint256 cantidadBFT = msg.value / precioToken;
         require(cantidadBFT > 0, "El ETH enviado es insuficiente");
+
         //Mint son tokens nuevos
         _mint(msg.sender, cantidadBFT * 10 ** 18);
+
     }
 
     function retirar() public onlyOwner {
+        console.log("Ejecutado por Luis Jose Flores Rodriguez");
         payable(owner()).transfer(address(this).balance);
-    }
+    } 
+
+
+
 }
